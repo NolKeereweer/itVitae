f7feba86c90685816de08c6a87ea95f6cc38d7ff
NolKeereweer <118395409+NolKeereweer@users.noreply.github.com>
2023-09-06 14:51:24 +0200

    Updated ExerciseSix.java
    fixed spacing

--- ExercisesWeekOneThruFive/ExerciseSix.java
+++ ExercisesWeekOneThruFive/ExerciseSix.java
@@ -3,7 +3,7 @@ public class ExerciseSix {
 	    String wordString = "Six";
 		
 		byte wholeNumberSmall = 66;
-        short wholeNumberMedium = 6666;
+                short wholeNumberMedium = 6666;
 		int wholeNumberLarge = 6666666;
 		long wholeNumberVeryLarge = 6666666666666L;
 		float fractionalNumberMedium = 6.66666F;
@@ -12,11 +12,11 @@ public class ExerciseSix {
 		char singleCharacter = 54;
 		
 		System.out.println("We've got strings: " + wordString + "\nAnd 8 more datatypes, those 8 are primitive");
-        System.out.println("They're the bytes: " + wholeNumberSmall +
-            "\nshorts: " + wholeNumberMedium + "\nintegers: " + wholeNumberLarge +
-            "\nlongs: " + wholeNumberVeryLarge + "\nfloats: " + fractionalNumberMedium +
-            "\ndouble: " + fractionalNumberLarge +
-            "\nbooleans, these can only be true or false: " + trueOrFalse +
-            "\nand chars: " + singleCharacter);
+                System.out.println("They're the bytes: " + wholeNumberSmall +
+                    "\nshorts: " + wholeNumberMedium + "\nintegers: " + wholeNumberLarge +
+                    "\nlongs: " + wholeNumberVeryLarge + "\nfloats: " + fractionalNumberMedium +
+                    "\ndouble: " + fractionalNumberLarge +
+                    "\nbooleans, these can only be true or false: " + trueOrFalse +
+                    "\nand chars: " + singleCharacter);
     }
 }
