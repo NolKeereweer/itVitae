321714194eaf7a4fba1cbe260cbfc36d565425e3
NolKeereweer <nol.keereweer202@gmail.com>
2023-09-06 12:38:40 +0200

    Exercise Four completed by way of args as input

---
+++ ExercisesWeekOneThruFive/ExerciseFourOptOne.java
@@ -0,0 +1,9 @@
+public class ExerciseFourOptOne {
+	public static void main(String[] args) {
+		String inputName = args[0];
+		
+		System.out.println("Bread is ready.\nWho is the bread for?");
+		System.out.println(inputName);
+		System.out.println("Noted: " + inputName + " got bread.");
+	}
+} 
