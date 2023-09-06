df9bcebffea4f926f53ff3bd680d3fd43badc678
NolKeereweer <nol.keereweer202@gmail.com>
2023-09-06 12:29:34 +0200

    Exercise Three completed, multiple println statements

---
+++ ExercisesWeekOneThruFive/ExerciseThree.java
@@ -0,0 +1,9 @@
+public class ExerciseThree {
+	public static void main(String[] args) {
+		System.out.println("I think that there");
+		System.out.println("Isn't");
+		System.out.println("A limit to");
+		System.out.println("The amount of statements...");
+		System.out.println("Maybe I just haven't found it yet");
+	}
+}
