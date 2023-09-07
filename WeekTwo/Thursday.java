f879195d8a3899f77472d7d38693ff4b4498eb03
NolKeereweer <nol.keereweer202@gmail.com>
2023-09-07 14:18:50 +0200

    Exercise to create a calculator for speed, ineffiect code but gets the job done

--- WeekTwo/Thursday.java
+++ WeekTwo/Thursday.java
@@ -4,16 +4,40 @@ public class Thursday {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
-        System.out.println("What temperature is it currently?");
+        System.out.println("Enter distance in meters:");
 
-        double temperatureFahrenheit = input.nextDouble();
+        double distanceInMeters = input.nextDouble();
 
-        System.out.println(temperatureFahrenheit + "? Oh sorry I don't speak cheeseburger per football field...");
+        System.out.println("Enter hours:");
+
+        double timeHours = input.nextDouble();
+
+        System.out.println("Enter minutes:");
+
+        double timeMinutes = input.nextDouble();
+
+        System.out.println("Enter seconds:");
+
+        double timeSeconds = input.nextDouble();
+
+        System.out.println("Calculating...");
         
-        input.close();
-        
-        double temperatureCelsius = (temperatureFahrenheit - 32) / 1.8;
+        input.close(); // everything above this line is for user input: meters and time
 
-        System.out.println("It is currently: " + temperatureCelsius + " degrees Centigrade.");       
+        double distanceInMiles = distanceInMeters / 1609; // converting meters to miles
+
+        double totalTimeInSeconds = timeSeconds + (timeMinutes * 60) + (timeHours * 60 * 60); // coverting hours and minutes into seconds
+        
+        double totalTimeInHours =  timeHours + (timeMinutes / 60) + (timeSeconds / 60 / 60); // converting minutes and seconds into hours
+        
+        double speedMetersSeconds = distanceInMeters / totalTimeInSeconds; // calculating speed in meters per seconds
+
+        double speedKilometersHours = speedMetersSeconds * 3.6; // calculating speed in kilometers per hour
+
+        double speedMilesHours = distanceInMiles / totalTimeInHours; // calculating speed in miles per hour
+
+        System.out.println("You have travelled a distance of: " + distanceInMiles + " miles at a speed of:\n");
+        System.out.println(speedMetersSeconds + " m/s\n" + speedKilometersHours + " km/h\n" + 
+                           speedMilesHours + " Mph"); // final print statement, shows speed in all 3 measurements + distance in miles
     }    
 }
