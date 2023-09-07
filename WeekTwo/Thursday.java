ffbb4e3e154e2ff3f9e334e3d9a92554f41cffcd
NolKeereweer <nol.keereweer202@gmail.com>
2023-09-07 13:09:58 +0200

    intro to BigIntegers and using longs

--- WeekTwo/Thursday.java
+++ WeekTwo/Thursday.java
@@ -1,6 +1,16 @@
+import java.math.*;
+import java.util.Scanner;
+
 public class Thursday {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("insert year count:");
 
+        long years = input.nextInt();
+        
+        System.out.println(Long.toUnsignedString(years * 60L * 365L * 24L));
+        input.close();
     }
     
 }
