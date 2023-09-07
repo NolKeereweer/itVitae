63cffb681884c73cb73c510e5c0bdd00807d39de
NolKeereweer <nol.keereweer202@gmail.com>
2023-09-07 18:48:48 +0200

    Started on STHMAS exercise, haven't figured it out quite yet

---
+++ ExercisesWeekOneThruFive/SecondsToHourMinutesAndSeconds.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+ 
+public class SecondsToHourMinutesAndSeconds {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int seconds = input.nextInt();
+        input.close();
+        System.out.println("Input seconds: " + seconds)
+        
+    }
+}
