f2bfc926b699b01a3386e9b82d6fd29f415d6710
NolKeereweer <nol.keereweer202@gmail.com>
2023-09-14 11:18:21 +0200

    Added commentary to the first exercises from thursday

--- WeekThree/ThursdayAssignmentOne.java
+++ WeekThree/ThursdayAssignmentOne.java
@@ -2,29 +2,28 @@ import java.util.Scanner;
 
 public class ThursdayAssignmentOne {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter your base: ");
-        
-        int base = input.nextInt();
+        Scanner input = new Scanner(System.in); // Opens a scanner to gather user input
 
-        System.out.print("Enter your exponentary value: ");
+        System.out.print("Enter your base: "); // First print line
+        int base = input.nextInt(); // First user input
+        System.out.print("Enter your exponentary value: "); // Second print line
+        int exp = input.nextInt(); // Second user input
+        System.out.println(base + " To the power of " + exp + " is " + exponent(base, exp)); /*
+           Final print statement that calls the exponent method to power the first and second inputs and print the answer
+        */
 
-        int exp = input.nextInt();
+        //------------------------------------------------------------------------------------------------------------------------//
 
-        System.out.println(base + " To the power of " + exp + " is " + exponent(base, exp));
-
-        System.out.println("Enter a number to see if it's odd: ");
-
-        int isItOdd = input.nextInt();
-
-        System.out.println(isOdd(isItOdd));
+        System.out.println("Enter a number to see if it's odd: "); // Print line second exercise to gather number
+        int isItOdd = input.nextInt(); // Input to gather number
+        System.out.println(isOdd(isItOdd)); // Print statement that calls the isOdd method and print the answer
     }
 
-    public static int exponent(int base, int exp) {
-        return (int) Math.pow(base, exp);
+    public static int exponent(int base, int exp) { // Method to power integers base and exp
+        return (int) Math.pow(base, exp); // Cast an int from a double, power the ints, immediatly returns answer
     }
 
-    public static boolean isOdd(int isItOdd) {
-        return isItOdd % 2 != 0;
+    public static boolean isOdd(int isItOdd) { // Method to check if int is odd
+        return isItOdd % 2 != 0; // Turning an int into a boolean by way of modulo is not 0, immediatly returns answer
     }
 }
