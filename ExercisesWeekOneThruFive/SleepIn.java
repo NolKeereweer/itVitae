d56fe2a69093683cee11b23148adac3af31d580d
NolKeereweer <nol.keereweer202@gmail.com>
2023-09-14 15:09:58 +0200

    Fixed sleepIn

--- ExercisesWeekOneThruFive/SleepIn.java
+++ ExercisesWeekOneThruFive/SleepIn.java
@@ -13,13 +13,13 @@ public class SleepIn {
         
         boolean vacationStatus;
         
-        if (vacation.equals("Yes")) {
+        if (vacation.equals("Yes")) { // check if it is vacation or not
             vacationStatus = true;
         } else {
             vacationStatus = false;
         }
         
-        ArrayList<String> workDays = new ArrayList<>();
+        ArrayList<String> workDays = new ArrayList<>(); // Creat a list with possible days to work/go to school on
         workDays.add("Monday");
         workDays.add("Tuesday");
         workDays.add("Wednesday");
@@ -28,19 +28,23 @@ public class SleepIn {
         
         boolean weekDay;
         
-        if (whatDay.equals(workDays)) {
+        if (whatDay.equals(workDays)) { // Check if it is a workday
             weekDay = true;
         } else {
             weekDay = false;
         }
         
-        if (weekDay == false || vacationStatus == true) {
-            sleepIn = true;
-        } else {
+        if (weekDay == false && vacationStatus == false) { // Check if it either vacation and/or a workday
             sleepIn = false;
+        } else if (weekDay == true && vacationStatus == true) {
+            sleepIn = true;
+        } else if (weekDay == true && vacationStatus == false) {
+            sleepIn = false;
+        } else {
+            sleepIn = true;
         }
         
-        if (sleepIn = true) {
+        if (sleepIn == true) { // Check if you can sleep in
             System.out.println("Let's sleep in a little longer");
         } else {
             System.out.println("Wake up! You're gonna be late.");
