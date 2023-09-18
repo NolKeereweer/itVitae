efa5bcec35be838c3bff066f2abe2db6b96515e0
NolKeereweer <nol.keereweer202@gmail.com>
2023-09-18 11:20:06 +0200

    Third iteration with a scanner

--- ExercisesWeekOneThruFive/Diff21.java
+++ ExercisesWeekOneThruFive/Diff21.java
@@ -1,11 +1,14 @@
+import java.util.Scanner;
+
 public class Diff21 {
     public static void main(String[] args) {
-        System.out.println(diff21(9));
-        System.out.println(diff21(12));
-        System.out.println(diff21(19));
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(diff21(n));
     }    
     private static int diff21 (int n) {
         int diff= Math.abs(21 - n);
+        System.out.print("Difference = ");
         return diff;
     }
 }
