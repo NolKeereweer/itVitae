25cf99ba5917210285f6707c86dc5e3488de2ff2
NolKeereweer <nol.keereweer202@gmail.com>
2023-09-18 11:10:04 +0200

    Iteration 2 of diff21, this is the way the solution did it

--- ExercisesWeekOneThruFive/Diff21.java
+++ ExercisesWeekOneThruFive/Diff21.java
@@ -1,8 +1,8 @@
 public class Diff21 {
     public static void main(String[] args) {
-        int n = 20;
-        
-        System.out.println("diff21(" + n + ") -> " + diff21(n));
+        System.out.println(diff21(9));
+        System.out.println(diff21(12));
+        System.out.println(diff21(19));
     }    
     private static int diff21 (int n) {
         int diff= Math.abs(21 - n);
