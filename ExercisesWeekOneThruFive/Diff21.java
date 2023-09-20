d9c04727c403d5d3056712d86cc7ad63cf95c77e
NolKeereweer <nol.keereweer202@gmail.com>
2023-09-20 09:28:35 +0200

    Fixed the Diff21 exercise

--- ExercisesWeekOneThruFive/Diff21.java
+++ ExercisesWeekOneThruFive/Diff21.java
@@ -3,12 +3,20 @@ import java.util.Scanner;
 public class Diff21 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.print("Enter a number: ");
         int n = in.nextInt();
         System.out.println(diff21(n));
     }    
     private static int diff21 (int n) {
-        int diff= Math.abs(21 - n);
-        System.out.print("Difference = ");
-        return diff;
+        int diff;
+        if (n <= 21) {
+            diff = Math.abs(21 - n);
+            System.out.print("Difference = ");
+            return diff;
+        } else {
+            diff = Math.abs((21 - n) * 2);
+            System.out.print("Difference = ");
+            return diff;
+        }
     }
 }
