b5597dbe9378aa49f9185a7637457136cdb1f282
NolKeereweer <nol.keereweer202@gmail.com>
2023-09-24 09:56:21 +0200

    Made some changes to the Hangman program

--- WeekFour/Hangman.java
+++ WeekFour/Hangman.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.awt.Toolkit;
 
 class Hangman {
   
@@ -9,41 +10,55 @@ class Hangman {
   private static final String ANSI_GREEN = "\u001B[32m"; // set text color green
   
   public static void main(String[] args) {
-    String magicWord; // THE word
-    Scanner input = new Scanner(System.in); // open scanner
+    String actualWord; 
+    Scanner input = new Scanner(System.in); 
+    int guessedCorrect = 0;
+    int guessedWrong = 0;
+    boolean guessedLetter = false;
     
     System.out.print("Enter your word: ");
-    magicWord = input.nextLine(); // assign input value to THE word
+    actualWord = input.nextLine(); // assign input value to THE word
     
-    ArrayList<Character> hiddenWord = new ArrayList<Character>(); // create ArrayList to hide THE word
+    ArrayList<Character> shownWord = new ArrayList<Character>(); // create ArrayList to hide THE word
     
-    for (int i = 0; i < magicWord.length(); i++) { // turn the characters in THE word to '_'
-      hiddenWord.add('_');
+    for (int i = 0; i < actualWord.length(); i++) { // turn the characters in THE word into '_'
+      shownWord.add('_');
     }
     
-    System.out.println("\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\nYou can guess ten times :)\ngo ahead.\n");
-    System.out.println(hiddenWord.toString()); // print the '_'s
+    System.out.println("\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\n-\nYou can guess wrong five times :)\ngo ahead.\n");
+    System.out.println(shownWord.toString()); // print the '_'s
     
-    int guessedCorrect = 0;
-    
-    for (int j = 0; j < 10; j++) { // giant for loop for the guesses
+    for (int j = 0; j < 15; j++) { // giant for loop for the guesses
       System.out.println("Guess a letter. ");
       char guess = input.next().charAt(0); // guessed character (via input)
       
-      for (int i = 0; i < magicWord.length(); i++) { // loops through THE word
-        if (magicWord.charAt(i) == guess && magicWord.charAt(i) != '_') { // check if the guess is valid
-          hiddenWord.set(i, guess); // changes the '_' to the guessed character
-          guessedCorrect++;
-        }
+      for (int i = 0; i < actualWord.length(); i++) { // loops through THE word
+        if (actualWord.charAt(i) == guess) {
+          guessedLetter = true;
+            if (shownWord.get(i) == '_') {
+              shownWord.set(i, guess);
+            } else {
+              break;
+            }
+        } 
       }
       
-      System.out.println(hiddenWord.toString()); // print the progress
+      if (guessedLetter) guessedCorrect++;
+      if (!guessedLetter) guessedWrong++;
       
-      if (j == 6) System.out.println(ANSI_RED + "Three tries left..." + ANSI_RESET);
+      System.out.println(shownWord.toString()); // print the progress
       
-      if (guessedCorrect == magicWord.length()) { // ends the program if the guesses are correct
+      if (guessedWrong == 4) System.out.println(ANSI_RED + "One msitake left..." + ANSI_RESET);
+      
+      if (guessedWrong == 5) {
+        System.out.println(ANSI_RED + "AHAHAHAHAHAH RIP BOZO L GEPAKT!!!!!" + ANSI_RESET);
+        break;
+      }
+      
+      if (guessedCorrect == actualWord.length()) { // ends the program if the guesses are correct
+        Toolkit.getDefaultToolkit().beep();
         System.out.println(ANSI_GREEN + "Success" + ANSI_RESET); 
-        System.exit(0);
+        break;
       }
     }
   }
