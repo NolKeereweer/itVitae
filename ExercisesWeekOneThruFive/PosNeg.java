31e334e55f1c95687a39464b2c3ed4aff7501d9e
NolKeereweer <nol.keereweer202@gmail.com>
2023-09-28 14:22:24 +0200

    Took another crack at the PosNeg excersise

--- ExercisesWeekOneThruFive/PosNeg.java
+++ ExercisesWeekOneThruFive/PosNeg.java
@@ -3,32 +3,17 @@ public class PosNeg {
     System.out.println(posNeg(1, -1, false));
     System.out.println(posNeg(-4, -5, true));
     System.out.println(posNeg(-1, 1, true));
+    System.out.println(posNeg(0, 0, true));
+    System.out.println(posNeg(0, 0, false));
+    System.out.println(posNeg(-5, 0, false));
+    System.out.println(posNeg(-5, 0, true));
   }
 
-  private static boolean posNeg(int a, int b, boolean negative) { // very lengthy boolean method
-    boolean bothNeg;
-    if (a <= 0 && b <= 0) { // statement to check if both numbers are negative
-      bothNeg = true;
+  private static boolean posNeg(int a, int b, boolean negative) { 
+    if (negative) { 
+      return a < 0 && b < 0;
     } else {
-      bothNeg = false;
+      return ((a < 0 && b > 0) || (a > 0 && b < 0));
     }
-
-    boolean singleNeg;
-    if (negative == true
-        && !bothNeg) { // statement to check if one number is negative and the parameter is true
-      singleNeg = true;
-    } else {
-      singleNeg = false;
-    }
-
-    boolean endNeg;
-    if (bothNeg) { // statement to finalise the response/return/output
-      endNeg = true;
-    } else if (singleNeg) {
-      endNeg = false;
-    } else {
-      endNeg = true;
-    }
-    return endNeg;
   }
 }
