7bdb175f304ca6ad36a43106db61ab8ac849c0ea
NolKeereweer <nol.keereweer202@gmail.com>
2023-10-19 12:09:28 +0200

    Made some changes to The Replicator to eliminate magic numbers. Thanks Raafi for the feedback!

--- ExercisesWeekOneThruFive/TheReplicator.java
+++ ExercisesWeekOneThruFive/TheReplicator.java
@@ -8,28 +8,36 @@ class TheReplicator {
     System.out.println("Enter 5 numbers to create your array: ");
     
     TextMethods.yellowText();
-    for (int i = 0; i <=4; i++) {
+    for (int i = 0; i < firstArray.length; i++) {
       firstArray[i] = input.nextInt();
     }
     TextMethods.resetText();
     
     System.out.println("\nArray created. \nReplicating... \nReplicating... \nReplicating...\n");
     
-    int[] replicatedArray = new int[5];    
+    int[] replicatedArray = new int[firstArray.length];    
     for (int i = 0; i <= 4; i++) {
       replicatedArray[i] = firstArray[i];
     }
     
     TextMethods.greenText();
     System.out.println("Original array: ");
-    for (int i = 0; i <= 4; i++) { 
-      System.out.print(firstArray[i] + ", ");    
+    for (int i = 0; i <= firstArray.length - 1; i++) {
+      if (i == firstArray.length - 1) {
+        System.out.print(firstArray[i]);    
+      } else {
+        System.out.print(firstArray[i] + ", ");
+      }
     }
     
     
     System.out.println("\n\nReplicated array: ");
-    for (int i = 0; i <= 4; i++) { 
-      System.out.print(replicatedArray[i] + ", ");    
+    for (int i = 0; i <= replicatedArray.length - 1; i++) { 
+      if (i == replicatedArray.length - 1) {
+        System.out.print(replicatedArray[i]);    
+      } else {
+        System.out.print(replicatedArray[i] + ", ");
+      }
     }
     TextMethods.resetText();
   }
