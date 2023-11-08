6773664c3214457e9e8e95228894878504a188f3
NolKeereweer <nol.keereweer202@gmail.com>
2023-11-08 15:47:28 +0100

    Using a derived query to find companies by name

--- SpringFase/databaseexcersise/src/main/java/data/v1/databaseexcersise/BedrijfRepository.java
+++ SpringFase/databaseexcersise/src/main/java/data/v1/databaseexcersise/BedrijfRepository.java
@@ -2,8 +2,9 @@ package data.v1.databaseexcersise;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
+import java.util.Set;
 
 @Component
 public interface BedrijfRepository extends JpaRepository <bedrijf, Long> {
-
+  Set<bedrijf> findByNaam(String name);
 }
