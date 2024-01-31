32e856b57e07bb2d148863ce12dcaf22b89a1bf4
NolKeereweer <nol.keereweer202@gmail.com>
2024-01-31 15:51:52 +0100

    Addition of the last missing objective. Two options delivered in file; a switch case, and an if-statement.

--- FirstFewWeeks/ExercisesWeekOneThruFive/Card.java
+++ FirstFewWeeks/ExercisesWeekOneThruFive/Card.java
@@ -27,19 +27,53 @@ public class Card {
     AMPERSAND;
   }
 
-  public Card(CardColor color, CardRank rank) {
+  private Card(CardColor color, CardRank rank) {
     this.color = color;
     this.rank = rank;
   }
 
   public String toString() {
-    return "The " + color + " " + rank + ".";
+    return "The " + color + " " + rank + " ";
+  }
+
+  private static void cardChecker(Card card) {
+//    switch(card.rank) {
+//      case ONE: 
+//      case TWO:
+//      case THREE:
+//      case FOUR:
+//      case FIVE:
+//      case SIX:
+//      case SEVEN:
+//      case EIGHT:
+//      case NINE:
+//      case TEN:
+//        System.out.println("is a number card.\n");
+//        break;
+//      case DOLLAR:
+//      case PERCENT:
+//      case CARET:
+//      case AMPERSAND:
+//        System.out.println("is a face card.\n");
+//        break;
+//      default:
+//        System.out.println("is not a viable card");
+//        break;
+//    }
+
+    if (card.rank == CardRank.DOLLAR || card.rank == CardRank.PERCENT 
+          || card.rank == CardRank.CARET || card.rank == CardRank.AMPERSAND) {
+      System.out.println("is a face card.\n");
+    } else {
+      System.out.println("is a number card.\n");
+    }
   }
 
   public static void main(String[] args) {
-    for (CardColor color : CardColor.values()/*each value in CardColor*/) {
-      for (CardRank rank : CardRank.values()/*each value in CardRank*/) {        
-        System.out.println(new Card(color, rank).toString().toLowerCase());
+    for (CardColor color : CardColor.values()) {
+      for (CardRank rank : CardRank.values()) {        
+        System.out.print(new Card(color, rank).toString().toLowerCase());
+        cardChecker(new Card(color, rank));
       }
     }
   }
